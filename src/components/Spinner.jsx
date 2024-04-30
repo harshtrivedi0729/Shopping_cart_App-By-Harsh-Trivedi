81c36881e898ec83bb97f9012293cbdcfdd05ab6
harshtrivedi0729 <24carat.harsh29@gmil.com>
2024-05-01 00:12:34 +0530

    Third Commit

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ import "./Spinner.css"
 const Spinner = () => {
   return (
     <div>
-      <div className='spinner' > </div>
+      <div className='spinner bg-red-600' > </div>
     </div>
   )
 }
